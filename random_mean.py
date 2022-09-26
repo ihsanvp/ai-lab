cee3ba02221af924b7e77335242c818f7e67df1d
Mohamed Ihssan <ihsanvp2738@gmail.com>
2022-09-26 14:40:37 +0530

    Add files via upload

---
+++ random_mean.py
@@ -0,0 +1,15 @@
+import numpy as np
+import random
+from math import sqrt
+
+data = []
+
+for i in range(10) :
+	data.append(random.randint(0, 100))
+
+mean = np.mean(data)
+sd = sqrt(np.var(data))
+
+print("data =", data)
+print("mean =", mean)
+print("standard deviation =", sd)
