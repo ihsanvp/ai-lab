a864e6cf6f235e0e98d7994fdae6b4a419cf15d2
Mohamed Ihssan <ihsanvp2738@gmail.com>
2022-10-31 15:39:43 +0530

    Add files via upload

--- histogram/program.py
+++ histogram/program.py
@@ -1,11 +1,21 @@
 import pandas as pd
 import statistics as st
+import numpy as np
+import matplotlib.pyplot as plt
 
-df = pd.read_csv("data.csv")
+df = pd.read_csv("data.csv", low_memory=False)
 
 #print(df.head())
 #print(df["SITE_NAME"])
-fl = df.query("SITE_NAME == 'Swale at Catterick Bridge'")
-mean_temp = fl['Oxygen dissolved continuous'].astype("int32", errors="ignore")
-print(fl.columns)
-print(mean_temp)
+df_new = df.query("SITE_NAME == 'Swale at Catterick Bridge'")
+
+t = df_new["Temperature water continuous"].astype(float)
+o = df_new["Oxygen dissolved continuous"].astype(float)
+
+print("mean of temperature =", np.mean(t))
+print("median of dissolved oxygen =", st.median(o))
+
+t.hist()
+plt.title("Histogram")
+plt.xlabel("Temperature")
+plt.show()
