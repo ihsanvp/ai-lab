e6e04c499c2ecd982ddc3ab433a8b79b0cc5740e
Mohamed Ihssan <ihsanvp2738@gmail.com>
2022-11-05 12:30:18 +0530

    Update decision_tree.py

--- decision-tree/decision_tree.py
+++ decision-tree/decision_tree.py
@@ -1,10 +1,8 @@
 import pandas as pd
 from sklearn import tree
 import matplotlib.pyplot as plt
-import seaborn as sns
 from sklearn.preprocessing import OneHotEncoder
 from sklearn.model_selection import train_test_split
-from sklearn import metrics
 
 df = pd.read_csv("car_evaluation.csv", names=["buying_price", "maint_cost", "doors", "person_capacity", "lug_boot", "safety", "class"], header=None)
 
@@ -15,10 +13,12 @@ X = df[input_features]
 Y = df[target_features]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3, random_state=2)
+
 enc = OneHotEncoder(sparse=False, handle_unknown="ignore")
 enc.fit(X_train)
 
 encoded_features = enc.get_feature_names_out(X_train.columns)
+
 X_train[encoded_features] = enc.transform(X_train)
 X_test[encoded_features] = enc.transform(X_test)
 
