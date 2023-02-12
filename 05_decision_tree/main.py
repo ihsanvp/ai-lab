2d2e06001c9a8e3d8cceb44a17ebb8e0e6b04b13
ihsanvp <ihsanvp2738@gmail.com>
2023-02-12 21:29:36 +0530

    start decision tree

--- 05_decision_tree/main.py
+++ 05_decision_tree/main.py
@@ -1,53 +1,26 @@
+from pathlib import Path
 import pandas as pd
-from sklearn import tree
-import matplotlib.pyplot as plt
 from sklearn.preprocessing import OneHotEncoder
-from sklearn.model_selection import train_test_split
 
-df = pd.read_csv("car_evaluation.csv",
+DATA_FILE = Path(__file__).parent / "car_evaluation.csv"
+
+df = pd.read_csv(DATA_FILE,
                  names=[
                      "buying_price", "maint_cost", "doors", "person_capacity",
                      "lug_boot", "safety", "class"
                  ],
                  header=None)
 
-input_features = df.columns[:-1]
-target_features = df.columns[-1]
+target_feature = "class"
+input_features = list(filter(lambda x: x != target_feature, df.columns))
 
 X = df[input_features]
-Y = df[target_features]
+Y = df[[target_feature]]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,
-                                                    Y,
-                                                    test_size=0.3,
-                                                    random_state=2)
+enc = OneHotEncoder().fit(X)
 
-enc = OneHotEncoder(sparse=False, handle_unknown="ignore")
-enc.fit(X_train)
+print(input_features)
+print(X.head())
+print(Y.head())
 
-encoded_features = enc.get_feature_names_out(X_train.columns)
-
-X_train[encoded_features] = enc.transform(X_train)
-X_test[encoded_features] = enc.transform(X_test)
-
-X_train.drop(columns=input_features, axis=1, inplace=True)
-X_test.drop(columns=input_features, axis=1, inplace=True)
-
-clf = tree.DecisionTreeClassifier(criterion="gini")
-clf.fit(X_train, Y_train)
-
-plt.figure(figsize=(120, 100))
-plt.title("Decision Tree Classification using Gini Index")
-tree.plot_tree(clf,
-               max_depth=10,
-               filled=True,
-               rounded=True,
-               feature_names=X_train.columns,
-               class_names=clf.classes_)
-
-print("train dataset size:", X_train.shape)
-print("test dataset size:", X_test.shape)
-print("accuracy of train :", clf.score(X_train, Y_train))
-print("accuracy of test :", clf.score(X_test, Y_test))
-
-plt.show()
+print(enc.transform(X))
